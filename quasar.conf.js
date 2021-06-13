fbf8d1158c27308cd606705a70fcd392b58b1759
Ruben <1455140+githrdw@users.noreply.github.com>
2021-06-13 14:21:55 +0200

    feat(theme): updated icons in template

--- quasar.conf.js
+++ quasar.conf.js
@@ -126,23 +126,18 @@ module.exports = configure(function (ctx) {
         theme_color: '#2f1b3f',
         icons: [
           {
-            src: 'icons/icon-128x128.png',
-            sizes: '128x128',
+            src: 'icons/icon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-32x32.png',
+            sizes: '32x32',
             type: 'image/png'
           },
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
             type: 'image/png'
           },
           {
