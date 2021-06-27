5c76aa334278c207365963e143159b4c925242ae
Ruben <1455140+githrdw@users.noreply.github.com>
2021-06-27 21:38:25 +0200

    feat(country): added Bianca's content

---
+++ src/components/content/bianca-north.vue
@@ -0,0 +1,182 @@
+<template>
+  <h2>Typografie voor verschillende doelgroepen</h2>
+  <p>
+    Ik vind het interessant hoe typografie gebruikt kan worden om een website
+    aan te kleden en daarom wilde ik hier dit blok meer over ontdekken en mee
+    experimenteren. <br />
+    Daarom bestond mijn eerste leerdoel uit het leren maken van verschillende
+    lay-outs (webpagina / one page) voor verschillende doelgroepen met alleen
+    typografie (wel vormen en kleuren). De content zou een sprookje bevatten en
+    het geheel is interessant om naar te kijken. Als oplevering zou ik een kort
+    onderzoek over typografie samenstellen en daarnaast 4 verschillende lay-outs
+    voor het sprookje ‘Goudlokje en de drie beren’ voor de
+    <strong>doelgroepen</strong>: kinderen t/m 11 jaar; jongeren 12 t/m 17 jaar;
+    volwassenen 18 t/m 74 jaar; en ouderen 75+.
+  </p>
+
+  <h3>Aanpak</h3>
+  <p>
+    Aan het begin van dit blok heb ik kort onderzoek gedaan naar welke sfeer en
+    uitstraling de verschillende typografieën hebben en heb ik geleerd dat
+    vanuit het verhaal zelf ook informatie gehaald kan worden. Het tempo, de
+    toon, de structuur en het doel geven al veel weg en dat moet met de
+    typografie <strong>ondersteund</strong> worden. Door deze onderdelen te
+    bepalen, kreeg ik al een beter beeld welk soort lettertype ik voor elke
+    doelgroep zou gebruiken. Naast de <strong>inspiratie</strong> die ik via
+    internet en kinderboeken die ik nog thuis had liggen heb gevonden, heb ik
+    vrij snel voor elke doelgroep eisen op kunnen stellen.
+  </p>
+  <figure>
+    <q-img src="../../assets/content/bianca-blok1.1-inspiratie.png" alt="Inspiratie" />
+    <q-chip
+      >Inspiratie afbeeldingen en eisen. Dit heb ik voor elke doelgroep op deze
+      manier gedaan.</q-chip
+    >
+  </figure>
+  <p>
+    Na genoeg inspiratie en informatie te hebben vergaard, ben ik begonnen met
+    het uitzoeken van geschikte lettertypes die ik heb gebruikt om te
+    <strong>experimenteren</strong> voor titels, paragrafen en gesproken tekst.
+    Hieronder staat een kleine selectie van deze experimenten.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.2-titels-paragrafen.png"
+      alt="Experimenten met titels, paragrafen en citaten"
+    />
+    <q-chip>Experimenten met titels, paragrafen en gesproken tekst</q-chip>
+  </figure>
+  <p>
+    Tegelijkertijd ben ik deze experimenten gaan samenvoegen om ook
+    verschillende lay-outs te proberen. Ik begon simpel met een effen
+    achtergrond, maar ben daarna patronen en verlopen gaan gebruiken om de
+    achtergrond interessanter te maken. Ook heb ik op internet de
+    <strong>trends</strong> van dit jaar opgezocht en heb deze voornamelijk
+    geprobeerd toe te passen op de titel. Een aantal van deze experimenten staan
+    hieronder. Te zien is dat is veel lettertypes, lay-outs en combinaties heb
+    geprobeerd voordat ik aan het einde van dit blok bij het resultaat ben
+    gekomen.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.3-paragrafen1.png"
+      alt="Experimenten"
+    />
+  </figure>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.4-paragrafen2.png"
+      alt="Experimenten"
+    />
+  </figure>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.5-paragrafen3.png"
+      alt="Experimenten"
+    />
+  </figure>
+  <p>
+    Om ervoor te zorgen dat de verschillende lay-outs ook echt bij de
+    doelgroepen pasten, heb ik naast het onderzoek, ook
+    <strong>getest</strong> met een aantal personen die passen binnen 2 van de
+    doelgroepen. Ik heb hen een aantal lettertypen en lay-outs voorgelegd die ik
+    heb uitgekozen aan de hand van de onderzoeksresultaten en inspiraties,
+    waarna zij hun voorkeuren hebben doorgegeven. Die voorkeuren heb ik gebruikt
+    om keuzes te maken over de lay-outs. Voor de andere 2 doelgroepen heb ik
+    vooral gebruik gemaakt van de onderzoeksresultaten en inspiraties. Verdere
+    feedback heb ik tijdens de wekelijkse lessen kunnen vragen aan docenten die
+    werkzaam zijn in dit vakgebied.
+  </p>
+
+  <h3>Resultaat</h3>
+  <p>
+    Het eindresultaat zijn 4 verschillende lay-outs van het sprookje ‘Goudlokje
+    en de drie beren’. Door minimale kleur toe te voegen, ligt de focus bij de
+    tekst en is de hele pagina een mooi geheel. De eerste lay-out voor de
+    jongste doelgroep heeft visuele aandachtspunten en variatie in kleur,
+    tekstgrootte en lettertype om het interessant en speels te maken.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.17-eind-kinderen.jpg"
+      alt="Het eindontwerp voor de doelgroep kinderen t/m 11 jaar."
+    />
+    <q-chip>Het eindontwerp voor de doelgroep kinderen t/m 11 jaar</q-chip>
+  </figure>
+  <p>
+    De tweede lay-out voor de jongeren is modern en strak met een diagonale
+    tekstrichting. Hier is geen variatie in kleur en zijn de visuele
+    aandachtspunten de vele hoofdletters aan het begin van elke paragraaf om
+    makkelijk door de tekst te scannen.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.18-eind-jongeren.jpg"
+      alt="Het eindontwerp voor de doelgroep jongeren 12 t/m 17 jaar."
+    />
+    <q-chip>Het eindontwerp voor de doelgroep jongeren 12 t/m 17 jaar</q-chip>
+  </figure>
+  <p>
+    De derde lay-out is gebaseerd op de nostalgie van de doelgroep en heeft
+    daarom een serif lettertype en klassieke dropcaps. De paragraven zijn
+    visueel gescheiden om rust te creëren in een drukke lay-out.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.19-eind-volwassenen.jpg"
+      alt="Het eindontwerp voor de doelgroep volwassenen 18 t/m 74 jaar."
+    />
+    <q-chip
+      >Het eindontwerp voor de doelgroep volwassenen 18 t/m 74 jaar</q-chip
+    >
+  </figure>
+  <p>
+    De vierde en laatste lay-out is gefocust op de tekst en de leesbaarheid
+    ervan. Een groter lettertype, ruststukken tussen de tekstblokken en een
+    simpele achtergrond zorgt al voor een gemakkelijk leesbare tekst.
+  </p>
+  <figure>
+    <q-img
+      src="../../assets/content/bianca-blok1.20-eind-ouderen.jpg"
+      alt="Het eindontwerp voor de doelgroep ouderen 75+."
+    />
+    <q-chip>Het eindontwerp voor de doelgroep ouderen 75+</q-chip>
+  </figure>
+
+  <h3>Reflectie</h3>
+  <p>
+    Mijn leerdoel van dit blok heb ik zeker behaald. Ik ben meer te weten
+    gekomen over soorten lettertypes, hun uitstraling, hoe ze gecombineerd
+    kunnen worden, enzovoort. Door het korte onderzoek ben ik meer te weten
+    gekomen over de verschillende lettertype, hoe ze te gebruiken en te
+    combineren en om de sfeer en inhoud van de tekst terug te laten zien in de
+    typografie. Deze resultaten heb ik toegepast in combinatie met inspiratie en
+    inbreng van de doelgroep. Hierdoor heb ik ervoor gezorgd dat de
+    verschillende lay-outs zo goed mogelijk aansluiten bij de juiste
+    doelgroepen.
+  </p>
+  <p>
+    De <strong>inzichten</strong> die ik tijdens dit blok heb opgedaan zijn meer
+    dan ik had verwacht. Ik heb niet alleen ontdekt op welke manieren typografie
+    gebruikt kan worden op een website en hiermee geëxperimenteerd, maar ook dat
+    ik niet altijd zomaar elk ontwerp kan overnemen die ik op internet vind. Elk
+    ontwerp is speciaal gemaakt om bij de inhoud en het doel van die tekst te
+    passen, dus dat kan niet zomaar worden overgenomen voor elk ander ontwerp.
+    Daarom moet ik dus eerst erachter komen wat de inhoud en het doel van míjn
+    tekst is en teksten zoeken die daarmee overeenkomen en die als inspiratie
+    gebruiken. Daarnaast is het niet altijd nodig om een heel uitbundige lay-out
+    te hebben. Voor sommige doelgroepen, zoals de kinderen en ouderen is het
+    juist verstandiger om een rustige lay-out te hebben zodat zij zich beter
+    kunnen focussen op de tekst.
+  </p>
+  <p>
+    Ik heb nu een beter beeld van de invloed van typografie en op welke manieren
+    ik typografie kan weergeven. Dus aan het einde van dit blok ben ik anders
+    naar typografie gaan kijken en erachter gekomen dat er zoveel meer achter
+    zit dan alleen maar een lettertype. Het wordt gebruikt om de inhoud van de
+    tekst te versterken en daarnaast een visueel aantrekkelijk beeld te maken.
+    Alles wat ik in dit blok heb geleerd zal ik meenemen tijdens toekomstige
+    projecten.
+  </p>
+   
+</template>
