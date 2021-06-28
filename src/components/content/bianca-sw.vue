d2b5d6be0f8306794344695e595d25a7134b053e
Ruben <1455140+githrdw@users.noreply.github.com>
2021-06-28 17:14:58 +0200

    feat(country): added Bianca's content

---
+++ src/components/content/bianca-sw.vue
@@ -0,0 +1,113 @@
+<template>
+  
+<h2>Geschaduwde illustraties in perspectief</h2>
+<p>
+	Ik vind het leuk om te illustreren, vooral realistisch, en wilde hier beter in worden door de tijd te nemen om hieraan te werken. Ik heb nog niet veel gebruik gemaakt van Adobe Illustrator, dus kon dat goed aan dit leerdoel koppelen.
+</p>
+<p>
+	Daarom bestond dit leerdoel uit het leren om <strong>digitaal te illustreren</strong> m.b.v. Adobe Illustrator waarbij ik me focus op licht, schaduw en perspectief. Voor deze technieken heb ik meerdere gereedschappen gebruikt en uiteindelijk mijn voorkeursgereedschappen voor elke techniek gekozen, die ik vervolgens heb gebruikt om het eindproduct te maken. Als oplevering zou ik met elke gereedschap dezelfde kleine illustraties maken die passen bij het sprookje ‘Goudlokje en de drie beren’. Als laatste zou ik deze eerdere kleine illustraties opnieuw maken met mijn gekozen techniek en deze verwerken in een grote eindillustratie.
+</p>
+<h3 class="q-my-lg">Aanpak</h3>
+<p>
+	Het begin van dit blok ben ik in LinkedIn Learning op zoek gegaan naar <strong>tips of tricks</strong> die me kunnen helpen om de 2 gekozen technieken te leren. Tegelijkertijd begon ik met proberen in Illustrator om te zien of ik nu al een voorwerp met <strong>licht en schaduw</strong> kan maken, specifieker een lepel. Dit zou de eerste, eenvoudigste illustratie zijn die bij het sprookje past.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.1-lepel.jpg" alt="Eerste experimenten licht en schaduw met een lepel" />
+	<q-chip>Eerste experimenten van licht en schaduw met een lepel</q-chip>
+</figure>
+<p>
+	Zoals hierboven te zien is, ging dit niet helemaal goed. Ik probeerde het met een gradient en met losse vormen, maar dat was geen succes. Daarom ben ik naar de gevonden video’s in LinkedIn Learning gaan kijken voor uitleg of tutorials over licht en schaduw. Daar had ik er gauw een gevonden en uit die <strong>tutorial</strong> heb ik een voor mij totaal nieuwe gereedschap gebruikt.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.2-tutorial.png" alt="Resultaat tutorial" />
+	<q-chip>Het resultaat na een tutorial over een gereedschap voor licht en schaduw</q-chip>
+</figure>
+<p>
+	Dit gereedschap heb ik vervolgens gebruikt om ook een bord/kom te maken en de 4 karakters van het sprookje. Ik pakte dit vrij snel op en bleef daarmee op schema.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.3-papbord.png" alt="Experiment borden" />
+	<q-chip>Experimenten met borden</q-chip>
+</figure>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.4-karakters.png" alt="4 karakters" />
+	<q-chip>De 4 karakters van het sprookje</q-chip>
+</figure>
+<p>
+	Ik wilde nog meer gereedschappen proberen om uiteindelijk een gereedschap te kiezen die voor mij het beste werkt, dus ben op internet naar mogelijkheden voor licht en schaduw gereedschappen gaan zoeken. Daar kwam ik er 4 tegen die ik allemaal heb uitgeprobeerd op een van de karakters. Na elke gereedschap geprobeerd te hebben, heb ik mijn voors en tegens opgeschreven en de gereedschappen vergeleken. Hieruit heb ik de tweede en derde gekozen als mijn favoriet, elk voor verschillende soorten illustraties. De tweede voor realistisch en de derde voor cartoons. Aangezien ik wil leren <strong>realistische illustraties</strong> te maken, koos ik voor het tweede gereedschap.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.5-technieken.png" alt="Voors en tegens licht en schaduw" />
+	<q-chip>De voors en tegens over elke gereedschap voor licht en donker</q-chip>
+</figure>
+<p>
+	Na de eerste 2 weken met licht en schaduw bezig te zijn geweest, ben ik de 2 weken daarop gaan focussen op <strong>perspectief</strong>. Eerst probeerde ik zelf een tafel in perspectief te maken, maar toen dat niet wilde lukken heb ik een voorbeeld van internet erbij gevonden die ik ernaast heb gehouden. Dit hielp heel erg.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.6-tafel-experiment.png" alt="Tafel in perspectief." />
+	<q-chip>Eerste probeersels met de tafel in perspectief. De middelste is gemaakt m.b.v. een voorbeeld</q-chip>
+</figure>
+<p>
+	In een van de eerder gevonden video’s leerde ik isometrie kennen. Ik dacht dat dit een soort perspectief was en vond het handig werken. De illustraties konden gemakkelijk verkleint, vergroot of verplaatst worden zonder dat het perspectief negatief beïnvloedt wordt. Dit ben ik gaan toepassen op elke illustratie die ik met perspectief wilde maken.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.7-isometrie.png" alt="Isometrie" />
+	<q-chip>Alle illustraties is isometrie</q-chip>
+</figure>
+<p>
+	Maar ik kwam er later pas achter dat isometrie eigenlijk een vals perspectief is, en ik heb deze illustraties opnieuw moeten maken met een echt perspectief met een ander gereedschap.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.8-perspectief.png" alt="Eecht perspectief" />
+	<q-chip>De isometrische illustraties opnieuw maken in echt perspectief</q-chip>
+</figure>
+<p>
+	Jammer genoeg kon ik maar 2 gereedschappen vinden voor perspectief. Met de ene gereedschap had ik meer moeite om overweg te komen dan met de andere, dus de keuze tussen deze 2 was redelijk makkelijk.
+</p>
+<p>
+	De laatste week heb ik de gekozen gereedschappen voor de 2 technieken gebruikt om mijn <strong>eindillustratie</strong> te maken. Ik begon natuurlijk met het schetsen van de scène en een achtergrond.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.9-schets.png" alt="Schets eindillustratie" />
+	<q-chip>De eerste schets voor de eindilustratie</q-chip>
+</figure>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.10-achtergrond.png" alt="Achtergrond eindillustratie" />
+	<q-chip>De achtergrond voor de eindillustratie gemaakt met het voorkeursgereedschap</q-chip>
+</figure>
+<p>
+	Het illustreren van de kleine illustraties ging heel goed. Ik had de gekozen gereedschappen al redelijk onder de knie en ik heb de illustraties al een aantal keren gemaakt in de afgelopen weken. Alleen de schaduwen van de stoelen vond ik iets lastiger, omdat er een aantal rare vormen inzaten met lastigere schaduwen. Maar het is wel gelukt. Ik heb na algemene feedback een trap toegevoegd om de verdiepingen visueel te verbinden en de uitsnede van de tussenvloer verbeterd t.o.v. wat te zien is bij de achtergrond en het perspectief van de blauwe borden heb ik ook kloppend gemaakt, maar nu is de wel onderverdieping af!
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.11-onderkant.png" alt="De onderverdieping" />
+	<q-chip>De onderverdieping is klaar</q-chip>
+</figure>
+<p>
+	Op de bovenverdieping gebruikte ik minder illustraties, omdat deze groter waren en de ruimte voldoende vulden. De schaduw van de bedden was het lastigst, omdat net als bij de stoelen er een aantal rare vormen bijzeten met lastige schaduwen. Hier heb ik ook besloten om ook een andere gereedschap te gebruiken naast maar mijn voorkeursgereedschap, om het goede resultaat te krijgen. Nu moest ik alleen de karakters nog maken en die een plek geven. De basis kon ik gelukkig gebruiken van de illustraties uit de eerste 2 weken. Het toevoegen van de schaduwen ging vrij vlot en de houdingen vond ik ook makkelijk te bepalen en vast te leggen.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.12-bovenkant.png" alt="De bovenverdieping" />
+	<q-chip>De bovenverdieping is klaar</q-chip>
+</figure>
+<p>
+	Door tijdens de wekelijkse lessen aan docenten <strong>feedback</strong> te vragen die werkzaam zijn in dit vakgebied, heb ik ervoor gezorgd dat ik de technieken steeds beter beheers. Zo ben ik een aantal keer geholpen, om perspectieven waar ik moeite mee had recht te zetten. Ook hebben ze me erop gewezen dat het volgen van tutorials goed is, zolang ik deze direct toepas om het uit te proberen. Als ze dit niet hadden gezegd, zou ik de eerste anderhalf week voornamelijk tutorials hebben opgezocht en bekeken.
+</p>
+<h3 class="q-my-lg">Resultaat</h3>
+<p>
+	Het eindresultaat is een grote illustratie waar de voorgaande kleinere illustraties in terugkomen. Ik had voor beide technieken mijn voorkeursgereedschap van Illustrator gebruikt, dus het illustreren ging redelijk snel en gemakkelijk. Er waren een aantal schaduwen die niet wilden lukken met de gekozen gereedschap, dus die heb ik met een ander gereedschap gedaan zodat het resultaat er goed uitziet.
+</p>
+<figure>
+	<q-img src="../../assets/content/bianca-blok2.13-eind.jpg" alt="De eindillustratie" />
+	<q-chip>De eindillustratie voor het sprookje ‘Goudlokje en de drie beren’</q-chip>
+</figure>
+<h3 class="q-my-lg">Reflectie</h3>
+<p>
+	Mijn leerdoel van dit blok heb ik wel <strong>behaald</strong>. Ik heb veel verschillende gereedschappen uitgeprobeerd, jammer genoeg niet allemaal even uitgebreid, en heb hier mijn eigen voorkeuren gekozen. Ik heb ook meer geleerd over de technieken licht, schaduw en perspectief en heb dit toegepast in een ontzettend leuk eindresultaat.
+</p>
+<p>
+	Tijdens het illustreren wilde ik eerst geen voorbeelden gebruiken, omdat ik het zelf wilde proberen te maken. Maar ik had al vrij snel door dat dit niet altijd lukte zoals ik wilde. Daarom heb ik uiteindelijk toch besloten om wel voorbeelden te gebruiken om de basis van sommige vormen over te kunnen nemen. Ook had ik van tevoren vastgesteld om in het eindresultaat voor elke techniek maar 1 gereedschap te gebruiken, omdat ik op die manier kan laten zien dat ik die gereedschap goed beheerd. Maar terwijl ik met de eindillustratie bezig was, merkte ik dat het in sommige gevallen bijna onmogelijk was om mijn voorkeursgereedschap te gebruiken. Ik heb er dus toen voor gekozen om ook een andere gereedschappen te gebruiken, omdat het niet anders kon. Door te combineren van technieken zijn het resultaat en het proces wel veel beter geworden. Ik ben blij dat ik uiteindelijk die keuze heb gemaakt.
+</p>
+<p>
+	Ik ben heel blij met het eindresultaat en wat ik over de technieken heb geleerd. In de toekomst zal ik makkelijker illustraties kunnen maken, omdat ik hier nu meer ervaring over heb opgedaan.
+</p>
+</template>
