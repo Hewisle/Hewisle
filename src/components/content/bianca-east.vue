4d6cf7e30d081a95ff3b5956c0cf3ddc23f0ea20
Ruben <1455140+githrdw@users.noreply.github.com>
2021-06-30 16:41:01 +0200

    feat(country): added Bianca's content

---
+++ src/components/content/bianca-east.vue
@@ -0,0 +1,184 @@
+<template>
+  <h2>Herontwerp met micro interacties</h2>
+  <p>
+    Animaties en interacties zijn mij steeds meer gaan fascineren. Door kleine
+    interacties toe te voegen aan een pagina, wordt het
+    <strong>interessanter en bewegelijker</strong> wat de aandacht van de
+    gebruiker kan trekken. Daarom heb ik voor een leerdoel gekozen waar ik dit
+    kan leren.
+  </p>
+  <p>
+    Het leerdoel bestond uit het leren maken van micro interacties en om deze
+    toe te voegen aan een mobiele website. Daarvoor heb ik eerst een herontwerp
+    gemaakt waar ik deze interacties aan toe kan voegen. Na 5 weken wilde ik een
+    prototype hebben in <strong>ProtoPie</strong> van een verbeterde
+    <a href="https://www.dierenwebshop.nl/" target="_blank">dierenwebshop.nl</a>
+    met micro interacties.
+  </p>
+  <h3>Aanpak</h3>
+  <p>
+    Ik had nog helemaal geen ervaring met ProtoPie, dus wilde voordat ik echt
+    aan de slag zou gaan een aantal <strong>tutorials</strong> volgen. Zo kon ik
+    zien wat ik er ongeveer mee zou kunnen maken en zo kreeg ik al een feel van
+    het programma. Het was aan het begin wel zoeken naar de functies en waar
+    alles stond, maar ik had na deze tutorials de basis al wel redelijk onder de
+    knie.
+  </p>
+  <iframe
+    src="https://youtu.be/3a9kgUy_Z0o"
+    name="Tutorial ProtoPie - verplaatsen"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/9k63K8aEN_E"
+    name="Tutorial ProtoPie - verschillende acties"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/GFHEF0gBYfo"
+    name="Tutorial ProtoPie - verticaal scrollen"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/Noobkv5KjAU"
+    name="Tutorial ProtoPie - tabbladen"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/n3W7Vu-c1AY"
+    name="Tutorial ProtoPie - variabele"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/HmCvFPuod0E"
+    name="Tutorial ProtoPie - variabele"
+  ></iframe>
+  <p>
+    Tegelijkertijd ben ik begonnen aan het <strong>herontwerp</strong> voor de
+    dierenwebshop. De kleuren van de huidige website zijn paars, maar ik vond
+    dat niet passen bij de content en de vormgeving. Daarom ben ik in de loop
+    van het proces steeds meer voor aardekleuren gegaan, omdat dat het beste
+    past bij de content. Ook heb ik een logo gemaakt en was ik bezig geweest met
+    componenten om toe te voegen op de pagina's. Voor het herontwerp, had ik de
+    basis van de huidige website overgenomen en aangepast met de nieuwe
+    huisstijl. Dit ging best goed en in ongeveer 2 weken was het herontwerp af.
+  </p>
+  <figure>
+    <q-img src="bianca-blok3.7-huidig.png" alt="Huidige website" />
+    <q-chip>De homepagina van de huidige website</q-chip>
+  </figure>
+  <figure>
+    <q-img src="bianca-blok3.9-styleguide.png" alt="Styleguide" />
+    <q-chip>Styleguide na veel iteratie</q-chip>
+  </figure>
+  <p>
+    Nadat ik het herontwerp af had, heb ik deze mockups geëxporteerd naar
+    ProtoPie om er <strong>micro interacties</strong> aan toe te voegen. Een
+    aantal interacties ging verrassend goed, zoals het menu, de lader, de
+    variabele prijzen en de prullenbak. Andere interacties, zoals het filter,
+    het bestelproces en de knoppen gingen iets moeizamer en daar heb ik veel
+    tijd aan besteed. Door video's op te zoeken en veel uit te proberen zijn
+    bijna alle interacties zo geworden zoals ik het wilde. Alleen de knoppen
+    zijn uiteindelijk niet helemaal gelukt. Hier moest ik stukjes code voor
+    gebruiken en door de vele lagen op een pagina ging dat niet heel goed. Ik
+    heb daarom de interactie eenvoudiger gemaakt, zodat er wel interactie is.
+    Hier volgt een lijst met video's van alle interacties (in sommige video's
+    zijn meerdere interacties te zien) die ik tijdens deze 5 weken heb gemaakt.
+  </p>
+  <iframe
+    src="https://youtu.be/bhY49zsUAXQ"
+    name="ProtoPie - micro interactie menu"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/QPNy8KG9EmE"
+    name="ProtoPie - micro interactie filter"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/Sxe0n5uaVTo"
+    name="ProtoPie - micro interactie lader"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/dUTsWaT1x80"
+    name="ProtoPie - micro interactie prullenbak"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/rlnDT24KPdQ"
+    name="ProtoPie - micro interactie prijzen variabele"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/TkjsNnBdLdU"
+    name="ProtoPie - micro interactie bestelproces"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/Rh-_gdec6xI"
+    name="ProtoPie - micro interactie in winkelwagen"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/s6SMUPBu-I0"
+    name="ProtoPie - micro interactie button"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/7lmq6ixLgG8"
+    name="ProtoPie - micro interactie chat"
+  ></iframe>
+  <iframe
+    src="https://youtu.be/65ZvpFgif9U"
+    name="ProtoPie - micro interactie zoekbalk"
+  ></iframe>
+  <p>
+    Om ervoor te zorgen dat het herontwerp en de micro interacties er zo goed
+    mogelijk uit zouden zien, heb ik tijdens de wekelijkse lessen
+    <strong>feedback</strong> gevraagd aan docenten die werkzaam zijn in dit
+    vakgebied. Zij hielpen mij om de juiste kleuren te kiezen en vertelden mij
+    wanneer een interactie iets aangepast kon worden om het nog beter te maken.
+    Ook wezen zij me erop dat het niet noodzakelijk is om tutorials te volgen,
+    maar gewoon te beginnen en tijdens het proces video’s te zoeken wanneer het
+    nodig was. Ik had wel van tevoren tutorials gevolgd, aangezien ik helemaal
+    niets van het programma wist. Ik ben blij dat ik dat wel heb gedaan,
+    tegelijkertijd met het maken van het herontwerp om mijn tijd zo nuttig
+    mogelijk te besteden. Als laatste hebben zij me geleerd dat micro
+    interacties subtiel moeten zijn en niet té opvallend, omdat het dan chaos
+    kan worden op een webpagina of app. Maar ze moeten ook wel te zien zijn,
+    anders voegt het niets toe.
+  </p>
+  <h3>Resultaat</h3>
+  <p>
+    Het resultaat is een leuke herontwerp van de website
+    <a href="https://www.dierenwebshop.nl/" target="_blank">dierenwebshop.nl</a>
+    gemaakt in Figma en met toegevoegde micro interacties in ProtoPie. Ik ben
+    heel blij met het resultaat en met de hoeveelheid die ik heb geleerd en
+    uitgeprobeerd in beide programma’s. (Let op! In het prototype hieronder
+    werkt niet álles, door Touch Hints is te zien waar op geklikt kan worden.)
+  </p>
+  <iframe
+    src="https://cloud.protopie.io/p/bce1c19c6d?ui=false&mockup=false&touchHint=true&scaleToFit=true&cursorType=touch"
+    width="25%"
+    height="80%"
+  ></iframe>
+  <h3>Reflectie</h3>
+  <p>
+    Mijn leerdoel van dit blok heb ik, vind ik zelf, met vlag en wimpel
+    <strong>behaald</strong>. Ik heb niet alleen een aantal micro interacties
+    leren maken, ik heb er meer gemaakt dan ik had verwacht. En ik vond het echt
+    heel erg leuk om te doen. Ik had graag nog meer interacties leren maken met
+    andere triggers dan klikken, maar daar had ik geen tijd meer voor. Misschien
+    ga ik hier in de <strong>toekomst</strong> nog wel verder mee en zal ik meer
+    in ProtoPie leren.
+  </p>
+  <p>
+    Ook heb ik geleerd dat het fijn werkt om problemen of ingewikkelde
+    interacties <strong>visueel</strong> te maken, zodat ik voor me kan zien wat
+    er gebeurd en hoe het interacteert. Dit heeft me uiteindelijk veel tijd en
+    chaos gescheeld. Daarnaast merkte ik dat ik zo enthousiast was om
+    interacties in ProtoPie te maken, dat ik het herontwerp soms iets rommelig
+    heb afgemaakt. Gelukkig werd ik hierop gewezen door de docenten, zodat het
+    eindproduct wel netjes eruit ziet.
+  </p>
+  <p>
+    Als laatste had ik in de eerste weken veel voorbeelden van micro interacties
+    gevonden die ik na wilde proberen te maken. Maar dit heb ik niet gedaan,
+    omdat ik zelf met ideeën kwam die ik wilde maken. Ik weet dus dat ik
+    voortaan de voorbeelden makkelijker bereikbaar moet hebben, zodat ik ze niet
+    kan vergeten. Of ik moet per project overwegen of ik minder tijd aan het
+    zoeken naar voorbeelden moet besteden, aangezien ik ze niet altijd gebruik.
+  </p>
+  <p>
+    Al om al ben ik heel erg blij met het resultaat en ben ik erachter gekomen
+    dat ik hier nog meer over wil leren en uitproberen.
+  </p>
+</template>
