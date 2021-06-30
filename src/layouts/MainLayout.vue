ce3931eb43751bffa98239c1977edf1252543907
Ruben <1455140+githrdw@users.noreply.github.com>
2021-06-30 15:16:41 +0200

    feat(space): some Safari fixes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -31,7 +31,7 @@ export default defineComponent({
   background-size: cover;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' viewBox='0 0 1920 1080'%3E%3Cdefs%3E%3CradialGradient id='a' cx='40%25' cy='52%25' r='70%25' gradientTransform='translate(.018 -.1) skewX(-10)'%3E%3Cstop stop-color='%23101033' offset='0'/%3E%3Cstop stop-color='%232F1B3F' offset='35%25'/%3E%3Cstop stop-color='%23101033' offset='75%25'/%3E%3Cstop stop-color='%23101033' offset='100%25'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect width='100%25' height='100%25' fill='url(%23a)' transform='scale(1.2)'/%3E%3C/svg%3E");
 }
-.background--stars{
+.background--stars {
   position: fixed;
   top: 0;
   bottom: 0;
@@ -47,6 +47,17 @@ export default defineComponent({
 .q-page-container::v-deep() .q-page {
   transition: min-height 0.5s ease 0s;
 }
+
+// Stoopid Safari does something weird on desktop with fill-available
+@media not all and (min-resolution: 0.001dpcm) and (min-width: 1025px) {
+  @supports (-webkit-appearance: none) and (stroke-color: transparent) {
+    .layout-wrapper,
+    .q-scrollarea {
+      height: 100% !important;
+    }
+  }
+}
+
 .layout-wrapper,
 .q-scrollarea {
   height: 100%;
