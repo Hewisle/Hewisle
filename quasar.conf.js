a270cf9f5bb7b5262962b6b1b5f10a6ecabbc83f
Ruben <1455140+githrdw@users.noreply.github.com>
2021-07-03 17:45:26 +0200

    feat(content): Dylan title

--- quasar.conf.js
+++ quasar.conf.js
@@ -57,9 +57,6 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      env: {
-        DOEDELZAK: (require('dotenv').config().parsed || {}).DOEDELZAK
-      },
 
       // transpile: false,
 
